bd9bff5e18b88fa2d78e63b47ac09a27bba97a21
AchaUmbaPretre <achandambi@gmail.com>
2023-07-26 14:46:34 +0100

    premier

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 import express from 'express'
 import cors from 'cors'
-import authRoute from './routes/auth.js'
-import postRoute from './routes/posts.js'
-import userRoute from './routes/users.js'
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -11,11 +8,9 @@ app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 
-app.use('/api/auth', authRoute)
-app.use('/api/posts', postRoute)
-app.use('/api/users', userRoute)
+
 
 
 app.listen(8080,()=>{
-    console.log('connecté')
+    console.log('Serveur est connecté')
 })
