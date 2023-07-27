97e1741ff2ad196d269d908453ed232bda7bcede
AchaUmbaPretre <achandambi@gmail.com>
2023-07-27 11:14:45 +0100

    commit

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -38,6 +38,37 @@ export const postEmploye = (req, res) => {
         if(error) res.status(500).json(error)
         return res.json('processus reussi');
     })
+}
 
-    
+export const deleteEmploye = (req, res)=>{
+    const employeId = req.params.id;
+    const q = "DELETE FROM employees WHERE id = ?"
+
+    db.query(q, [employeId], (err, data)=>{
+        if (err) return res.send(err);
+      return res.json(data);
+    })
+}
+
+export const updateEmploye = (req, res)=> {
+    const employeId = req.params.id;
+    const q = "UPDATE employees SET `first_name`= ?, `last_name`= ?, `date_of_birth`= ?, `gender`= ?, `address`= ?,`phone_number`= ?, `email`= ?, `identification_number`= ?, `identification_type`= ?,`skills`= ?, `certifications`= ?, `employment_status`= ? WHERE id = ?"
+    const values = [
+        req.body.first_name,
+        req.body.last_name,
+        req.body.date_of_birth,
+        req.body.gender,
+        req.body.address,
+        req.body.phone_number,
+        req.body.email,
+        req.body.identification_number,
+        req.body.identification_type,
+        req.body.skills,
+        req.body.certifications,
+        req.body.employment_status
+    ];
+    db.query(q, [...values,employeId], (err, data) => {
+        if (err) return res.send(err);
+        return res.json(data);
+      });
 }
--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getEmploye, postEmploye } from "../controllers/adminControllers.js";
+import { deleteEmploye, getEmploye, postEmploye, updateEmploye } from "../controllers/adminControllers.js";
 const router = express.Router()
 
 router.get('/', getEmploye)
 router.post('/employe', postEmploye)
-
+router.delete('/employe/:id', deleteEmploye)
+router.put('/employe/:id', updateEmploye)
 export default router
